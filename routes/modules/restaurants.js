9ad0b261fc4bbb29a3a08f6e46d76c9c757fc700
Rick Wu <rickwu316@gmail.com>
2021-01-17 20:28:30 +0800

    feat: restful router: combine restaurant funciton to restaurants

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -2,7 +2,28 @@ const express = require('express')
 const router = express.Router()
 // 引用 Todo model
 const restaurantList = require('../../models/restaurant')
-// 定義首頁路由
+
+router.get('/', (req, res) => {
+
+    const sortType = req.query.sortType
+    const sort = {  //設定排序邏輯
+        AtoZ: { name: 'asc' },
+        ZtoA: { name: 'desc' },
+        location: { location: 'asc' },
+        category: { category: 'asc' },
+    }
+
+    // console.log(sort[sortType])
+
+    restaurantList.find() // 取出 Todo model 裡的所有資料
+        .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+        .sort(sort[sortType])
+        .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+        .catch(error => console.error(error)) // 錯誤處理
+
+})
+
+//show
 router.get('/:restaurant_id', (req, res) => {
     const id = req.params.restaurant_id
     return restaurantList.findById(id)
@@ -56,12 +77,41 @@ router.delete('/:restaurant_id', (req, res) => {
         .catch(error => console.log(error))
 })
 
-// 匯出路由模組
+//search
+router.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+
+
+    console.log(req.query)
+    return restaurantList.find({ 'name': { '$regex': keyword, '$options': 'i' } })
+        .lean()
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch(error => console.log(error))
+
+})
+
+
+//create
+router.get('/new', (req, res) => {
+    const restaurant = req.body
+    res.render('createRestaurant')
+})
+
+router.post('/new', (req, res) => {  //把物件ID跟User關聯
+    const userId = req.user._id
+    const restaurant = req.body
+    User.findById(userId)
+        .then((user) => {
+            restaurantList.create(restaurant)
+                .then(restaurant => {
+                    user.authority.push(restaurant._id.toString())
+                    user.save()
+                })
+        })
+        .then(() => res.redirect('/'))
+})
+
+
+
+
 module.exports = router
-
-
-
-
-
-
-
